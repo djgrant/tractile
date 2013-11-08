f9629abb6e0703806907aa3aa46913d08a2f8238
Daniel Grant <danieljohngrant@gmail.com>
2013-11-08 12:02:56 +0000

    Restrucutre assets

--- js/examples.js
+++
@@ -1,9 +0,0 @@
-Tractile($('.Tractile'), {
-  interval: 4000
-});
-
-// Tractile(document.querySelectorAll('.Tractile'), {
-//   interval: 5000
-// });
-
-// Tractile(document.querySelector('.Tractile'));
