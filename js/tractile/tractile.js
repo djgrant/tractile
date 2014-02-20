e1491017a915e672fda843ffef6acd8416ee2b5f
Daniel John Grant <danieljohngrant@gmail.com>
2014-02-20 11:20:52 +0000

    Remove redundant check

--- js/tractile/tractile.js
+++ js/tractile/tractile.js
@@ -13,10 +13,6 @@
       }
     }
 
-    if (this === window) {
-      return;
-    }
-
     node = (node instanceof jQuery) ? node[0] : node;
 
     if (!node.children || node.children.length < 2) {
